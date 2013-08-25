b714e4f189f1801c6e5db66145a37a54eda88f6d
saki92 <sakthi92@gmail.com>
2013-08-25 14:03:05 +0530

    crated tle update file

--- tlecomp.py
+++ tlecomp.py
@@ -6,28 +6,25 @@ x=0
 with open('tleupdate.txt') as l:
 	for i,n in enumerate(l):
 		if x==0:
-			print n
-			if n=='SRMSAT':
+			if 'SRMSAT' in n:
 				tle.append(n)
 				x=i
-				continue
 		else:
-			if x==i+1:
-				tle.append(n)
+			tle.append(n)
 		if len(tle)==3:
 			break
 
-print tle[0]
+#print tle[0], tle[1], tle[2]
 
 gs=ephem.Observer()
 gs.lat=13.064103
 gs.lon=80.218753
 
-"""while True:
+while True:
 #	gs.date=datetime.datetime.now()+datetime.timedelta(0,700)
 	gs.date=datetime.datetime.now()
 	sat=ephem.readtle(tle[0],tle[1],tle[2])
 	sat.compute(gs)
 	print sat.az, sat.alt
-"""
+
 
